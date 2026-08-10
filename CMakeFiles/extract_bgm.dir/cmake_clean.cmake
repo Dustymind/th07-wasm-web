519c236b4ce2440a05d6b887c0af9c1c4d679ed2
Dustymind <dustymind@foxmail.com>
2026-08-10 20:58:42 +0800

    remove unnecessary files, rebundle thbgm.data
        since standalone ogg files is f**king buggy

--- CMakeFiles/extract_bgm.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/extract_bgm"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/extract_bgm.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
